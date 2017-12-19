ec3cb8f175d24caa303cbbe6cee2ccb3f584d7a8
Víctor Castaño Gutiérrez <victorcastanogutierrez@gmail.com>
2017-12-19 20:10:11 +0100

    CORS, configuración, cambio en preguntas

--- src/main/java/com/pale/model/Pregunta.java
+++ src/main/java/com/pale/model/Pregunta.java
@@ -1,47 +1,45 @@
 package com.pale.model;
 
-import javax.annotation.Generated;
-import java.util.Objects;
-import java.util.HashSet;
-import javax.persistence.Table;
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.UniqueConstraint;
-import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-@Generated(
-	value = "DieselsCodeGenerator",
-	comments = "Generated business service class",
-	date = "Wed Dec 13 16:41:28 CET 2017"
-)
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Generated(value = "DieselsCodeGenerator", comments = "Generated business service class", date = "Wed Dec 13 16:41:28 CET 2017")
 @Entity
-@Table(name = "T_PREGUNTAS", uniqueConstraints = @UniqueConstraint(columnNames = {"texto"}))
+@Table(name = "T_PREGUNTAS", uniqueConstraints = @UniqueConstraint(columnNames = { "titulo" }))
 public class Pregunta extends BaseEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Column(name = "texto", nullable = false)
-	private String texto;
+
+	@Column(name = "titulo", nullable = false)
+	private String titulo;
+
+	@Column(name = "descripcion", nullable = false)
+	private String descripcion;
+
 	@OneToMany(mappedBy = "pregunta")
 	private Set<Respuesta> respuestas = new HashSet<>();
-	
-	Pregunta() {}
-	
-	public Pregunta(String texto) {
+
+	Pregunta() {
+	}
+
+	public Pregunta(String titulo) {
 		super();
-		this.texto = texto;
+		this.titulo = titulo;
 	}
-	
-	public String getTexto() {
-		return this.texto;
-	}
-	
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.getTexto());
+		return Objects.hash(this.getTitulo());
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -51,21 +49,36 @@ public class Pregunta extends BaseEntity implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Pregunta other = (Pregunta) obj;
-		return Objects.equals(this.getTexto(), other.getTexto());
+		return Objects.equals(this.getTitulo(), other.getTitulo());
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Pregunta [texto=" + this.getTexto() + "]";
+		return "Pregunta [texto=" + this.getTitulo() + "]";
 	}
-	
-	
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 	Set<Respuesta> _getRespuestas() {
 		return this.respuestas;
 	}
-	
+
 	public Set<Respuesta> getRespuestas() {
 		return new HashSet<>(respuestas);
 	}
-	
+
 }
--- src/main/java/com/pale/ws/TestRestController.java
+++ src/main/java/com/pale/ws/TestRestController.java
@@ -13,35 +13,37 @@ import com.pale.model.Pregunta;
 import com.pale.model.Respuesta;
 
 @RestController
-public class TestRestController { 
-	
+public class TestRestController {
+
 	@RequestMapping(value = "/preguntas", method = RequestMethod.GET)
 	public List<Pregunta> getPreguntas() {
 		List<Pregunta> preguntas = new ArrayList<>();
-		Pregunta p1 = new Pregunta("Â¿pregunta 1?");
-		Pregunta p2 = new Pregunta("Â¿pregunta 2?");
-		
+		Pregunta p1 = new Pregunta("¿pregunta 1?");
+		p1.setDescripcion("Descripción de la pregunta numero 1");
+		Pregunta p2 = new Pregunta("¿pregunta 2?");
+		p2.setDescripcion("Descripción de la pregunta numero 2");
+
 		Respuesta r1 = new Respuesta("Respuesta 1");
 		Respuesta r2 = new Respuesta("Respuesta 2");
 		Respuesta r3 = new Respuesta("Respuesta 3");
-		
+
 		Associations.Contiene.link(p1, r1);
 		Associations.Contiene.link(p1, r2);
 		Associations.Contiene.link(p1, r3);
-		
+
 		Associations.Contiene.link(p2, r1);
 		Associations.Contiene.link(p2, r2);
-		Associations.Contiene.link(p2, r3);	
-		
+		Associations.Contiene.link(p2, r3);
+
 		preguntas.add(p1);
 		preguntas.add(p2);
-		
+
 		return preguntas;
 	}
-	
+
 	@RequestMapping(value = "/coche", method = RequestMethod.GET)
 	public Coche pendingActivities() {
 		return new Coche("BMW", "i8");
 	}
-	
+
 }
